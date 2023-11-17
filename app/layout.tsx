733e68357ba92e16584e22f0215e7c4320acad0e
Piarre <pierre@ideestore.fr>
2023-11-17 21:59:21 +0100

    release

---
+++ app/layout.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import Header from "@/components/header";
+import "./globals.css";
+import { Inter } from "next/font/google";
+import ActiveSectionContextProvider from "@/context/active-section-context";
+import Footer from "@/components/footer";
+import ThemeSwitch from "@/components/theme-switch";
+import ThemeContextProvider from "@/context/theme-context";
+import { Toaster } from "react-hot-toast";
+import { useEffect, useState } from "react";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Pierre IDE",
+  description: "a 16 yo french dev.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, [mounted]);
+
+  return (
+    <html lang="en" className="!scroll-smooth">
+      <body
+        className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
+      >
+        <div
+          className={`bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-40rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#9984D4] ${
+            mounted ? "opacity-100" : "opacity-0"
+          }  transition-opacity delay-200 duration-1000 ease-in-out`}
+        ></div>
+        <div
+          className={`bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#cc5e45] ${
+            mounted ? "opacity-100" : "opacity-0"
+          }  transition-opacity delay-500 duration-1000 ease-in-out`}
+        ></div>
+        <div
+          className={`bg-[#fbe2e3] absolute top-[10rem] -z-10 right-[0rem] h-[30rem] w-[10rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#791E94] ${
+            mounted ? "opacity-100" : "opacity-0"
+          }  transition-opacity delay-700 duration-1000 ease-in-out`}
+        ></div>
+
+          <ThemeContextProvider>
+            <ActiveSectionContextProvider>
+            <Header />
+            {children}
+            <Footer />
+
+            <Toaster position="top-right" />
+            {/* <ThemeSwitch /> */}
+          </ActiveSectionContextProvider>
+        </ThemeContextProvider>
+      </body>
+    </html>
+  );
+}
---
+++ lib/data.ts
@@ -0,0 +1,120 @@
+import React from "react";
+import { CgWorkAlt } from "react-icons/cg";
+import { FaReact } from "react-icons/fa";
+import { LuGraduationCap } from "react-icons/lu";
+import mozintImg from "@/public/mozint.png";
+import kitchenntteImg from "@/public/kitchennette.png";
+
+export const links = [
+  {
+    name: "Home",
+    hash: "#home",
+  },
+  {
+    name: "About",
+    hash: "#about",
+  },
+  {
+    name: "Projects",
+    hash: "#projects",
+  },
+  {
+    name: "Skills",
+    hash: "#skills",
+  },
+  // {
+  //   name: "Experience",
+  //   hash: "#experience",
+  // },
+  {
+    name: "Contact",
+    hash: "#contact",
+  },
+] as const;
+
+export const experiencesData = [
+  {
+    title: "Discord & Spigot",
+    location: "Paris, FR",
+    description:
+      "I started coding with Java at 13 years old. I made Minecraft plugins and Discord bots. I also learned HTML, CSS and JavaScript after.",
+    icon: React.createElement(LuGraduationCap),
+    date: "2019 - 2020",
+  },
+  {
+    title: "Front-End Developement",
+    location: "Paris, FR",
+    description:
+      "Early 2020, I started learning Vue.js to start real Front-End development.",
+    icon: React.createElement(CgWorkAlt),
+    date: "2020",
+  },
+  {
+    title: "More Front-End and Back-End Development",
+    location: "Paris, FR",
+    description:
+      "At the beginning of 2021, I started learning Java and Angular to create Full-Stack apps.",
+    icon: React.createElement(FaReact),
+    date: "2021",
+  },
+  {
+    title: "Full-Stack and Mobile Development",
+    location: "Paris, FR",
+    description:
+      "I started learning TypeScript, React, Next.JS, Docker and more. I also started learning Swift to create iOS apps.",
+    icon: React.createElement(FaReact),
+    date: "2022",
+  },
+  {
+    title: "More langages and technologies",
+    location: "Paris, FR",
+    description: "",
+    icon: React.createElement(FaReact),
+    date: "2023 - Present",
+  },
+] as const;
+
+export const projectsData = [
+  {
+    title: "M-Ozint",
+    description:
+      "I worked on a full-stack WebApp, to help OSINTers and investigators to find information about people, companies, etc...",
+    tags: ["React", "Next.js", "GormDB", "Tailwind", "Flask", "Go", "ChakraUI"],
+    imageUrl: mozintImg,
+  },
+  {
+    title: "Kitchennette",
+    description:
+      "My mom had a damaged book with coffee on it and I decided to create a iOS app that save her recipes.",
+    tags: ["Switf", "SwitfUI", "Firebase"],
+    imageUrl: kitchenntteImg,
+  },
+] as const;
+
+export const skillsData = [
+  "TypeScript",
+  "JavaScript",
+  "Go",
+  "PowerShell",
+  "Swift",
+  "Java",
+  "Python",
+  "Bash",
+  "Spring",
+  "Vite",
+  "React",
+  "Next.js",
+  "Angular",
+  "Tailwind",
+  "Node.js",
+  "GitHub",
+  "Git",
+  "Docker",
+  "Electron",
+  "Express",
+  "Prisma",
+  "MongoDB",
+  "MySQL",
+  "Firebase",
+  "Postman",
+] as const;
