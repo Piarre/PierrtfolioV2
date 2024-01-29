4316decac5d0e5e94959b20a43c55c8aa3b675f0
Piarre <pierre@ideestore.fr>
2024-01-29 21:57:38 +0100

    :heavy_plus_sign: Add new types for projects and links

--- lib/types.ts
+++ lib/types.ts
@@ -1,3 +1,16 @@
 import { links } from "./data";
 
 export type SectionName = (typeof links)[number]["name"];
+
+export type Link = {
+  name: string;
+  hash: string;
+};
+
+export interface Project {
+  title: string;
+  description: string;
+  link?: Link | Link[];
+  tags: string[];
+  imageUrl?: any;
+}
